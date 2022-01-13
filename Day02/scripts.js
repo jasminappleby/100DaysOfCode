e6b04163e46b408f9ba5d7ed9fbb4098b5cfca70
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-13 10:40:15 +0000

    added all elements that will be used

---
+++ Day02/scripts.js
@@ -0,0 +1,12 @@
+const wordElement = document.getElementById("word");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainButton = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+const finalMessageRevealWord = document.getElementById(
+  "final-message-reveal-word"
+);
+const figureParts = document.querySelectorAll(".figure-part");
+
+
