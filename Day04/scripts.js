5d867a24107a1b7a40204c138867e9203dda7be5
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-15 11:16:41 +0000

    added default cards

--- Day04/scripts.js
+++ Day04/scripts.js
@@ -9,3 +9,22 @@ const answerElement = document.getElementById("answer");
 const addCardButton = document.getElementById("add-card");
 const clearButton = document.getElementById("clear");
 const addContainer = document.getElementById("add-container");
+
+
+let currentActiveCard = 0;
+const cardsElement = [];
+
+const cardsData = [
+    {
+        question: "How does I use this website?",
+        answer: "Write a question on the front, and write the answer on the back!",
+    },
+    {
+        question: "What if I put a card in incorrectly?",
+        answer: "You can clear all cards and start again.",
+    },
+    {
+        question: "What does HTML stand for?",
+        answer: "Hypertext Markup Language",
+    },
+];
