7936756fce4f0daae3e581f8c318ab0c137aff53
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-19 11:00:56 +0000

    literally like all the code

---
+++ Day08/scripts.js
@@ -0,0 +1,110 @@
+//Sorry if this makes your computer lag
+
+let canvas = document.getElementById("canvas");
+let c = canvas.getContext("2d");
+let tx = window.innerWidth;
+let ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+
+let mousex = 0;
+let mousey = 0;
+
+//ball will grow if hovered over
+
+addEventListener("mousemove", function() {
+  mousex = clientX;
+  mousey = clientY;
+});
+
+let grav = 0.99;
+c.strokeWidth=5;
+
+//randomises rgba code so every ball has a different colour on every refresh!
+
+function randomColour() {
+  return (
+    "rgba(" +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.round(Math.random() * 250) +
+    "," +
+    Math.ceil(Math.random() * 10) / 10 +
+    ")"
+  );
+}
+
+//creates ball using the random colour and a random circle radius
+
+function Ball() {
+  this.color = randomColour();
+  this.radius = Math.random() * 20 + 14;
+  this.startradius = this.radius;
+  this.x = Math.random() * (tx - this.radius * 2) + this.radius;
+  this.y = Math.random() * (ty - this.radius);
+  this.dy = Math.random() * 2;
+  this.dx = Math.round((Math.random() - 0.5) * 10);
+  this.vel = Math.random() /5;
+  this.update = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    c.fillStyle = this.color;
+    c.fill();
+  };
+}
+
+//adds 500!! balls to the page
+
+let bal = [];
+for (let i=0; i<500; i++){
+    bal.push(new Ball());
+}
+
+
+//animates the balls so they appear to bounce
+
+function animate() {    
+  if (tx != window.innerWidth || ty != window.innerHeight) {
+    tx = window.innerWidth;
+    ty = window.innerHeight;
+    canvas.width = tx;
+    canvas.height = ty;
+  }
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, tx, ty);
+  for (let i = 0; i < bal.length; i++) {
+    bal[i].update();
+    bal[i].y += bal[i].dy;
+    bal[i].x += bal[i].dx;
+    if (bal[i].y + bal[i].radius >= ty) {
+      bal[i].dy = -bal[i].dy * grav;
+    } else {
+      bal[i].dy += bal[i].vel;
+    }    
+    if(bal[i].x + bal[i].radius > tx || bal[i].x - bal[i].radius < 0){
+        bal[i].dx = -bal[i].dx;
+    }
+    if(mousex > bal[i].x - 50 && 
+      mousex < bal[i].x + 20 &&
+      mousey > bal[i].y -50 &&
+      mousey < bal[i].y +50 &&
+      bal[i].radius < 70){
+        bal[i].radius +=5; 
+      } else {
+        if(bal[i].radius > bal[i].startradius){
+          bal[i].radius += -5;
+        }
+      }
+      
+    }
+}
+
+animate();
+
+setInterval(function() {
+  bal.push(new Ball());
+  bal.splice(0, 1);
+}, 400);
+
