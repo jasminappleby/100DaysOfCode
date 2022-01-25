fc908c4d797e499cb920f7e778672564c936e1fa
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-25 09:28:07 +0000

    added some nice css and finished js

--- Day14/scripts.js
+++ Day14/scripts.js
@@ -1,3 +1,18 @@
 const change = document.getElementById('change');
 const colour = document.getElementById('colour');
 const body = document.body;
+
+change.addEventListener('click', changeBG);
+
+function changeBG() {
+	const col1 = getRandomRGB();
+	const col2 = getRandomRGB();
+	const col3 = getRandomRGB();
+	const colourString = `rgb(${col1}, ${col2}, ${col3})`;
+	body.style.background = colourString;
+	colour.innerText = colourString;
+}
+
+function getRandomRGB() {
+	return Math.floor(Math.random() * 256);
+}
