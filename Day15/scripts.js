76e19822e85dd7353d2a64d9ff979622c0fc0194
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-26 10:38:45 +0000

    added keypress event

--- Day15/scripts.js
+++ Day15/scripts.js
@@ -2,3 +2,14 @@ const tagsEl = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
 
+textarea.addEventListener('keyup', (e) => {
+	createTags(e.target.value);
+	
+	if(e.key === 'Enter') {
+		setTimeout(() => {
+			e.target.value = '';
+		}, 10)
+		
+		randomSelect();
+	}
+});
