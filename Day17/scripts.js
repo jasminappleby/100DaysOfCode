6cd111317013004de8783231d84e0f4cee10a9e1
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-28 08:37:50 +0000

    added css with floating hearts

--- Day17/scripts.js
+++ Day17/scripts.js
@@ -1,6 +1,9 @@
 const countEl = document.getElementById('count');
+const body = document.body;
 
+setInterval(createFlower, 50);
 updateVisitCount();
+
 
 function updateVisitCount() {
 	fetch('https://api.countapi.xyz/hit/jasmin-appleby-holdhamnetlifyapp/?amount=1')
@@ -8,4 +11,21 @@ function updateVisitCount() {
 	.then(res => {
 		countEl.innerHTML = res.value;
 	})
+}
+
+
+function createFlower() {
+	const flower = document.createElement('i');
+	flower.classList.add('far');
+	flower.classList.add('fa-heart');
+	flower.style.left = Math.random() * window.innerWidth + 'px';
+	flower.style.animationDuration = Math.random() * 3 + 2 + 's';
+	flower.style.opacity = Math.random();
+	flower.style.fontSize = Math.random() * 10 + 10 + 'px';
+	
+	document.body.appendChild(flower);
+	
+	setTimeout(() => {
+		flower.remove();
+	}, 5000)
 }
