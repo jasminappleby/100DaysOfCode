f0aa53357246933078b9abcac91b56697ded672d
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-30 10:27:10 +0000

    had a faff around with css, cant get names to be capatilsed and typing to colur the card bg

--- Day19/scripts.js
+++ Day19/scripts.js
@@ -8,28 +8,30 @@ const fetchPokemons = async () => {
 };
 
 const getPokemon = async id => {
-  const url =`https://pokeapi.co/api/v2/pokemon/${id}`;
-  const res = await fetch(url);
-  const pokemon = await res.json();
-  createPokemonCard(pokemon);
+    const url =`https://pokeapi.co/api/v2/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokemonCard(pokemon);
 }
 
 const createPokemonCard = (pokemon) => {
-  const pokemonEl = document.createElement('div');
-  pokemonEl.classList.add('pokemon');
-  const { id, name, sprites, types } = pokemon;
-  const type = types[0].type.name;
-  const pokeInnerHTML = `
-  <div class="img-container">
-    <img src="${sprites.front_default}" alt="${name}" />
-  </div>
-  <div class="info">
-    <span class="number">${id}</span>
-    <h3 class="name">${name}</h3>
-    <small class="type">Type: <span>${type}</span></small>
-  </div>
-  `;
-  pokemonEl.innerHTML = pokeInnerHTML;
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+    const { id, name, sprites, types } = pokemon;
+    const type = types[0].type.name;
+
+    const pokeInnerHTML = `
+    <div class="img-container">
+        <img src="${sprites.front_default}" alt="${name}" />
+    </div>
+    <div class="info">
+        <span class="number">${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>
+    `;
+    pokemonEl.innerHTML = pokeInnerHTML;
 	poke_container.appendChild(pokemonEl);
 }
 
