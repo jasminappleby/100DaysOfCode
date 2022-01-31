7b5170fab90cff740543cc732780e936e6d1f650
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-01-31 09:37:19 +0000

    removed name function because it wasnt working how i want

--- Day20/scripts.js
+++ Day20/scripts.js
@@ -2,19 +2,6 @@
 let player1 = "Player 1";
 let player2 = "Player 2";
 
-// Function to change the player name
-function editNames() {
-    player1 = prompt("Change Player1 name");
-    player2 = prompt("Change player2 name");
-
-    document.querySelector("p.Player1")
-                    .innerHTML = player1;
-                      
-    document.querySelector("p.Player2")
-                    .innerHTML = player2;
-}
-
-// Function to roll the dice
 function rollTheDice() {
     setTimeout(function () {
         let randomNumber1 = Math.floor(Math.random() * 6) + 1;
