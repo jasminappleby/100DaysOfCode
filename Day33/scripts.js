7711671049c73d02d4cee3725727bf71de348c4e
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-02-13 19:49:54 +0000

    i had to revert to ES5

--- Day33/scripts.js
+++ Day33/scripts.js
@@ -1,50 +1,54 @@
 window.onload = function () {
-    var seconds = 00;
-    var mili = 00;
-    var addSeconds = document.querySelector(".seconds");
-    var addMili = document.querySelector(".mili");
-    var startBtn = document.querySelector(".start");
-    var stopBtn = document.querySelector(".stop");
-    var resetBtn = document.querySelector(".reset");
-    var interval;
   
-    startBtn.onclick = function () {
-      clearInterval(interval);
-      interval = setInterval(start, 10);
-    };
+    var seconds = 00; 
+    var tens = 00; 
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
+    var Interval ;
   
-    stopBtn.onclick = function () {
-      clearInterval(interval);
-    };
-  
-    resetBtn.onclick = function () {
-      clearInterval(interval);
-      seconds = "00";
-      mili = "00";
-      addSeconds.innerHTML = seconds;
-      addMili.innerHTML = mili;
-    };
-  
-    function start() {
-      mili++;
-  
-      if (mili > 9) {
-        addMili.innerHTML = mili;
-      }
-      if (mili < 9) {
-        addMili.innerHTML = "0" + mili;
-      }
-  
-      if (mili > 99) {
-        console.log(seconds);
-        seconds++;
-        addSeconds.innerHTML = "0" + seconds;
-        mili = 0;
-        addMili.innerHTML = "0" + 0;
-      }
-  
-      if (seconds > 9) {
-        addSeconds.innerHTML = seconds;
-      }
+    buttonStart.onclick = function() {
+      
+      clearInterval(Interval);
+       Interval = setInterval(startTimer, 10);
     }
-  };
+    
+      buttonStop.onclick = function() {
+         clearInterval(Interval);
+    } 
+  
+    buttonReset.onclick = function() {
+       clearInterval(Interval);
+      tens = "00";
+        seconds = "00";
+      appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+    
+    function startTimer () {    
+        tens++; 
+        
+        if(tens <= 9){
+            appendTens.innerHTML = "0" + tens;
+        }
+        
+        if (tens > 9){
+            appendTens.innerHTML = tens;
+            
+        } 
+        
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+        
+        if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+        }
+    }   
+}
