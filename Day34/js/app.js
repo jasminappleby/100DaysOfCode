db98fa9529dca71eeae6910a0812b72b27d2ed02
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-02-14 10:01:04 +0000

    added animate feature but i think now ill make a start on another js file

--- Day34/js/app.js
+++ Day34/js/app.js
@@ -4,8 +4,28 @@ canvas.width = 600;
 canvas.height = 400;
 
 let spacePressed = false,
-  gameSpeed = 2,
-  angle = 0,
-  hue = 0,
-  frame = 0,
-  score = 0;
+    gameSpeed = 2,
+    angle = 0,
+    hue = 0,
+    frame = 0,
+    score = 0;
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bird.update();
+    bird.draw();
+    ctx.fillStyle = "red";
+    ctx.font = "90px Georgia";
+    ctx.strokeText(score, 450, 70);
+    ctx.fillText(score, 450, 70);
+    handlePar();
+    handlePip();
+    collide();
+    if (collide()) return;
+    requestAnimationFrame(animate);
+    angle += 0.2;
+    hue++;
+    frame++;
+}
+
+    animate();
