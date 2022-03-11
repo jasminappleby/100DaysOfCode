7acb93aa3b7d3a897641bef041bc3266bb37b566
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-03-11 12:17:37 +0000

    js for day 58

--- Day58/scripts.js
+++ Day58/scripts.js
@@ -18,7 +18,35 @@ new Vue({
             };
         },
 
+        mounted() {
+        this.cardNumberTemp = this.otherCardMask;
+        document.getElementById("cardNumber").focus();
+        },
+
+        computed: {
+
+        getCardType () {
+            let number = this.cardNumber;
+            let re = new RegExp("^4");
+            if (number.match(re) != null) return "visa";
+    
+            re = new RegExp("^(34|37)");
+            if (number.match(re) != null) return "amex";
+    
+            re = new RegExp("^5[1-5]");
+            if (number.match(re) != null) return "mastercard";
+    
+            re = new RegExp("^6011");
+            if (number.match(re) != null) return "discover";
+            
+            re = new RegExp('^9792')
+            if (number.match(re) != null) return 'troy'
+    
+            return "visa"; 
+        },
+
         
+    }
 });
   
   
