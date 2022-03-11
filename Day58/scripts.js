2db83b4a179eb0a6e334c5f058b7b0ecd4295fe6
jasminappleby <jasminapplebyholdham@hotmail.com>
2022-03-11 12:18:01 +0000

    js for day 58

--- Day58/scripts.js
+++ Day58/scripts.js
@@ -45,7 +45,49 @@ new Vue({
             return "visa"; 
         },
 
+            generateCardNumberMask () {
+                return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+        },
+    
+        minCardMonth () {
+            if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
+            return 1;
+        }
+        },
+    
+        watch: {
+        cardYear () {
+            if (this.cardMonth < this.minCardMonth) {
+            this.cardMonth = "";
+            }
+        }
+    },
         
+        methods: {
+        flipCard (status) {
+            this.isCardFlipped = status;
+        },
+        
+        focusInput (e) {
+            this.isInputFocused = true;
+            let targetRef = e.target.dataset.ref;
+            let target = this.$refs[targetRef];
+            this.focusElementStyle = {
+            width: `${target.offsetWidth}px`,
+            height: `${target.offsetHeight}px`,
+            transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
+            }
+        },
+        
+        blurInput() {
+            let vm = this;
+            setTimeout(() => {
+            if (!vm.isInputFocused) {
+                vm.focusElementStyle = null;
+            }
+            }, 300);
+            vm.isInputFocused = false;
+        }        
     }
 });
   
